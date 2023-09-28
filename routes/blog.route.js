a67c93eef981fdd6ad568d24fb725879c806017f
Kirti Bhosale <bhosalekirti2002@gmail.com>
2023-09-28 20:33:07 +0530

    middleware

--- routes/blog.route.js
+++ routes/blog.route.js
@@ -7,16 +7,15 @@ const {
   likeBlog,
   commentOnBlog,
 } = require("../controllers/blog.controller");
-const { authenticateUser } = require("../middlewares/auth.middleware");
 const blogRouter = Router();
 
-blogRouter.post("/blogs", authenticateUser, createBlog);
-blogRouter.get("/blogs", authenticateUser, getAllBlogs);
+blogRouter.post("/blogs", createBlog);
+blogRouter.get("/blogs", getAllBlogs);
 // blogRouter.get("/blogs/search", searchByTitle);
 // blogRouter.get("/blogs/category", getByCategory);
 // blogRouter.get("/blogs", sortByDateAsc);
-blogRouter.patch("/blogs/:id", authenticateUser, updateBlog);
-blogRouter.delete("/blogs/:id", authenticateUser, deleteBlog);
+blogRouter.patch("/blogs/:id", updateBlog);
+blogRouter.delete("/blogs/:id", deleteBlog);
 blogRouter.patch("/blogs/:id/like", likeBlog);
 blogRouter.patch("/blogs/:id/comment", commentOnBlog);
 
